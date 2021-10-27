8c5731a8b200b953e67d62558318c9f239f8212e
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-27 16:13:15 +0100

    entity

--- src/main/java/com/ecommerce/shop/data/model/Product.java
+++ src/main/java/com/ecommerce/shop/data/model/Product.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -14,12 +16,18 @@ public class Product {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "Name cannot be blank")
+    @NotNull(message = "Name cannot be null")
     private String name;
+    @NotNull(message = "Price cannot be null")
     private Double price;
 
 
     @Enumerated(EnumType.STRING)
+    @NotNull(message = "Currency cannot be null")
     private Currency currency;
+    @NotBlank(message = "Details cannot be blank")
+    @NotNull(message = "Details cannot be null")
     private String details;
 
     @ElementCollection()
