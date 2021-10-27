1203b2719ffc41dc85e3b4bfdde19aa3ce7de587
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-27 16:12:05 +0100

    service impl

--- src/main/java/com/ecommerce/shop/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/shop/service/ProductServiceImpl.java
@@ -1,17 +1,27 @@
 package com.ecommerce.shop.service;
 
+import com.ecommerce.shop.data.dto.ProductUpdateDto;
+import com.ecommerce.shop.data.model.Currency;
 import com.ecommerce.shop.data.model.Product;
 import com.ecommerce.shop.data.repository.ProductRepository;
+import com.ecommerce.shop.service.mapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.security.InvalidParameterException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    ProductMapper productMapper;
 
     @Override
     public Product save(Product product) {
@@ -36,5 +46,20 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public void deleteById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public Product updateProduct(Long id, ProductUpdateDto productDto) {
+        if(productDto == null){
+            throw new NullPointerException("Product DTO cannot be null");
+        }
+        Product product = findById(id);
+        if(product != null){
+            productMapper.mapDtoToProduct(productDto, product);
+            return productRepository.save(product);
+        }
+        throw new InvalidParameterException("Invalid product id");
+
     }
 }
